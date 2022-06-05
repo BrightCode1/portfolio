25f0be948fc469d4a7de2a2523c60220e527beb5
brightcode1 <brightonline80@gmail.com>
2022-06-05 09:09:04 +0100

    initial

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export { default as Contact } from "./Contact";
+export { default as Info } from "./Info";
+export { default as Navbar } from "./Navbar";
+export { default as Skills } from "./Skills";
