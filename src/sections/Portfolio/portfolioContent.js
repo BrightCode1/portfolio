af86f0420488771c11010e4ce758fa56ff5b5ecc
brightcode1 <brightonline80@gmail.com>
2022-07-10 15:32:24 +0100

    portfolio touch

--- src/sections/Portfolio/portfolioContent.js
+++ src/sections/Portfolio/portfolioContent.js
@@ -3,20 +3,21 @@ import Albaama from "../../assets/img/albaama.png";
 export const portfolioList = [
   {
     id: 0,
-    img: "https://play-lh.googleusercontent.com/SvSGmjbErolaNRqq3xqx6NFn3Rldjqz_SFvqRZQ5ElujeYVbP2k21M_C8lAdu28Hq60=w240-h480",
-    title: "Food Delivery Application",
-    desc: "An ecommerce food delivery mobile aplication <br /> skills: <b>Flutter, dart, APIs</b>",
-    link: "https://play.google.com/store/apps/details?id=ng.foodnow",
-    isAvailable: true,
-  },
-  {
-    id: 1,
     img: "https://play-lh.googleusercontent.com/7yraXXwHYZhILCctORn3PcTzIIqdARICxXY07hTIF637_lHAqdt3aQpXOPmYTZH76yk=w240-h480-rw",
     title: "Student Social Application",
     desc: "A social app where students are able to post, comment, communicate with each other, upload and download pdfs etc <br /> skills: <b>Flutter, dart, APIs, PHP, MySql, Firebase</b>",
     link: "https://play.google.com/store/apps/details?id=com.kcib.quelib",
     isAvailable: true,
   },
+  {
+    id: 1,
+    img: "https://play-lh.googleusercontent.com/SvSGmjbErolaNRqq3xqx6NFn3Rldjqz_SFvqRZQ5ElujeYVbP2k21M_C8lAdu28Hq60=w240-h480",
+    title: "Food Delivery Application",
+    desc: "An ecommerce food delivery mobile aplication <br /> skills: <b>Flutter, dart, APIs</b>",
+    link: "https://play.google.com/store/apps/details?id=ng.foodnow",
+    isAvailable: true,
+  },
+
   {
     id: 2,
     img: Albaama,
